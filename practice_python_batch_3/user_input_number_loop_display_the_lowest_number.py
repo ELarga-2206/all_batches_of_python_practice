7e5e132bf7fa432e0e64bb220cff83af1373c001
Elarga-2206 <earl.larga22@gmail.com>
2025-03-12 20:04:41 +0800

    added pseudo code

--- practice_python_batch_3/user_input_number_loop_display_the_lowest_number.py
+++ practice_python_batch_3/user_input_number_loop_display_the_lowest_number.py
@@ -1,1 +1,10 @@
 #Create a program that ask user to input a number, continue asking until the user input is invalid. Display the lowest number
+
+#same process
+#initialize
+#function
+#while loop
+#exit condition
+#input handling
+#append function
+#find the lowest number input

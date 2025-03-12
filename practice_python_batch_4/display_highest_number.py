f8b8064d524f86d6a7c054ac075ef8ba8119b552
Elarga-2206 <earl.larga22@gmail.com>
2025-03-12 22:48:06 +0800

    reusing the code from a similar problem in batch 3, modify later

--- practice_python_batch_4/display_highest_number.py
+++ practice_python_batch_4/display_highest_number.py
@@ -1,1 +1,24 @@
 # Create a program that ask user to input a number, continue asking until the user input is invalid. Display the highest number
+
+numbers = []
+
+print("type 'l' to display the lowest number")
+
+while True:
+    user_input = input("Enter a number: ")
+
+    if user_input == 'l':
+        break
+
+    try:
+        num = int(user_input)
+        numbers.append(num)
+
+    except ValueError:
+        print("Invalid input, enter a valid number.")
+    
+if numbers:
+    lowest_number = min(numbers)
+    print(f"The lowest number is: {lowest_number}")
+else:
+    print("No valid number")

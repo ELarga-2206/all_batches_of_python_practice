b3c688e8aa34aa134f95841f472374ea00ac0d0f
Elarga-2206 <earl.larga22@gmail.com>
2025-03-13 00:29:14 +0800

    added pseudo code for expected code guideline

--- practice_python_batch_4/display_number_that_have_duplicate.py
+++ practice_python_batch_4/display_number_that_have_duplicate.py
@@ -1,1 +1,18 @@
 # Create a program that ask user to input 10 numbers. Display all numbers that have duplicate.
+#collect numbers from user
+
+#check for duplicates (for i in range)
+#use (set) function to keep track of numbers
+#how to check for duplicates?
+#must create a duplicate checker
+
+numbers = []
+
+for i in range(1,11):
+    num = int(input("enter a number: "))
+    numbers.append(num)
+
+print("No duplicates: ")
+for num in numbers:
+    if numbers.count(num) == 1:
+        print(num)

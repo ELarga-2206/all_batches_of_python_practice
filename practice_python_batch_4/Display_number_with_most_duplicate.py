f7623e8933785d4b10e791c379b841624dc7413e
Elarga-2206 <earl.larga22@gmail.com>
2025-03-13 01:30:10 +0800

    set instructions, and exit condition for the while loop

--- practice_python_batch_4/Display_number_with_most_duplicate.py
+++ practice_python_batch_4/Display_number_with_most_duplicate.py
@@ -1,7 +1,11 @@
 # Create a program that ask user to input a number, continue asking until the user input is invalid. Display the number with the most number of duplicate.
 
-#instructions
-#while loop
-#user input
-#find the most duplicates using max.numbers?
-#display the no with most duplicates
+numbers = []
+
+print("type 'd' to display number with most duplicate")
+
+while True:
+    user_input = input("Enter a number: ")
+
+    if user_input == 'd':
+    break

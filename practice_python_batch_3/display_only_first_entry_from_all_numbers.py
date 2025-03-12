6c430afd38b60ab5dc4df39595725363c99a5f29
Elarga-2206 <earl.larga22@gmail.com>
2025-03-12 16:10:42 +0800

    updated program in batch 3, finished and tested

--- practice_python_batch_3/display_only_first_entry_from_all_numbers.py
+++ practice_python_batch_3/display_only_first_entry_from_all_numbers.py
@@ -1,2 +1,17 @@
-#Create a program that ask user to input 10 numbers. Display all numbers. For numbers with duplicate, display only the first entry.
+# Create a program that ask user to input 10 numbers. Display all numbers. For numbers with duplicate, display only the first entry.
 
+numbers = []
+
+for i in range(1,11):
+    num = int(input("enter a number: "))
+    numbers.append(num)
+
+unique = []
+
+for num in numbers:
+    if num not in unique:
+        unique.append(num)
+
+print("numbers with no duplicates:")
+for num in unique:
+    print(num)

d48c93b7eaf295f59f4195bfefaa0b65a5930300
Elarga-2206 <earl.larga22@gmail.com>
2025-03-13 01:23:44 +0800

    added a pseudocode for guide

--- practice_python_batch_4/Display_number_with_most_duplicate.py
+++ practice_python_batch_4/Display_number_with_most_duplicate.py
@@ -1,1 +1,7 @@
 # Create a program that ask user to input a number, continue asking until the user input is invalid. Display the number with the most number of duplicate.
+
+#instructions
+#while loop
+#user input
+#find the most duplicates using max.numbers?
+#display the no with most duplicates

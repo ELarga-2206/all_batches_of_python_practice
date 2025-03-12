3e55877576a014acc0138c4322a1f06aab6296a8
Elarga-2206 <earl.larga22@gmail.com>
2025-03-12 21:54:36 +0800

    added a pseudocode for clear identfication of requirements

--- practice_python_batch_4/display_average.py
+++ practice_python_batch_4/display_average.py
@@ -1,1 +1,13 @@
 # Create a program that ask user to input a number, continue asking until the user input is invalid. Display the average.
+
+#average
+# user inputs + user inputs / no of user inputs
+# must have counter of user inputs
+# must add the number to the list and update the total sum
+
+# counter = 0
+#while loop
+#user input
+#append valid inputs, then add their values
+#once added should be divided to total of inputs (valid numbers only, no alphabet)
+

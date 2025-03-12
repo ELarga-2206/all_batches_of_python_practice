f78929fb7a09885a9f7a19ec3386293f4ec7fabb
Elarga-2206 <earl.larga22@gmail.com>
2025-03-12 18:35:44 +0800

    pseudocode, initial idea

--- practice_python_batch_3/user_input_number_loop_display_from_lowest_to_highest.py
+++ practice_python_batch_3/user_input_number_loop_display_from_lowest_to_highest.py
@@ -1,1 +1,24 @@
 #Create a program that ask user to input a number, continue asking until the user input is invalid. Display the number from lowest to highest. Clue: sort() function
+
+#initialize list
+#user input
+#while loop
+#it must sort
+#no invalid inputs
+
+print("Enter numbers (type 'exit' to stop):")
+
+while True:
+    user_input = int(input("Enter a number: "))
+    if user_input == "exit":
+        break
+
+try:
+    num = user_input = int #must be converted to integer
+#append function
+    numbers.append(num)
+
+#how do we sort?
+
+
+

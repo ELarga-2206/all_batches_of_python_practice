5fc4f4f2b2acc02b4b8f34aea6801d154e4cbb08
Elarga-2206 <earl.larga22@gmail.com>
2025-03-12 20:31:23 +0800

    initial code idea, fix later

--- practice_python_batch_3/user_input_number_loop_display_the_lowest_number.py
+++ practice_python_batch_3/user_input_number_loop_display_the_lowest_number.py
@@ -8,3 +8,27 @@
 #input handling
 #append function
 #find the lowest number input
+
+number = []
+
+while True:
+    user_input = input("Enter a number: ")
+
+    if user_input == 'l':
+        break
+
+ try:
+    num = int(user_input)
+    numbers.append(num)
+
+    except ValueError:
+        print("Invalid input! Please enter a valid number.")
+    
+    if numbers:
+    low_num = min(numbers)
+    print("The lowest number is: {low_num}")
+else:
+    print("No valid num")
+
+#fix indentions bro wyd
+print("type 'l' to display the lowest number")

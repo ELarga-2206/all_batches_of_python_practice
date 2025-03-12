a3e3a6aa23ea2d11041ce8cde2796b1210cbaaa2
Elarga-2206 <earl.larga22@gmail.com>
2025-03-12 14:17:10 +0800

    fixed submodule issue annd re-added files correctly

---
+++ practice_python_batch_3/display_all_numbers_with_no_duplicate.py
@@ -0,0 +1,12 @@
+#Create a program that ask user to input 10 numbers. Display all numbers that don't have duplicate.
+
+numbers = []
+
+for i in range(1,11):
+    num = int(input("enter a number: "))
+    number.append(num)
+
+
+#add test
+
+#add another

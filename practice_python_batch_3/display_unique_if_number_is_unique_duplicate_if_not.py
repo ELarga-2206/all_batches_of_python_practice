5836f021cd6a568bd7d19592410795972f28ad78
Elarga-2206 <earl.larga22@gmail.com>
2025-03-12 16:32:37 +0800

    added pseudocode for batch 3 program

--- practice_python_batch_3/display_unique_if_number_is_unique_duplicate_if_not.py
+++ practice_python_batch_3/display_unique_if_number_is_unique_duplicate_if_not.py
@@ -1,3 +1,13 @@
 #Create a program that ask user to input a number, continue asking until the user input is invalid. Display "Unique" after input when the inputted number don't have duplicate. Display "Duplicate" after input when the inputted number have duplicate.
 
-print("add nothing this is ass bro.")
+#initialize empty list
+#use infinite loop/while loop
+#ask for user number input
+#check for duplicates in user input:
+    #print "duplicate"
+#check for unique numbers in user input:
+    #print "unique"
+#check if the user input is invalid:
+#end
+
+Start

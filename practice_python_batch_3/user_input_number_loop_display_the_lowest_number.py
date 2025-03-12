bd232b77a53eb3d248f01909a83967ce30870c8e
Elarga-2206 <earl.larga22@gmail.com>
2025-03-12 20:59:37 +0800

    fixed the min() function to display lowest number, fixed indent, replaced a variable

--- practice_python_batch_3/user_input_number_loop_display_the_lowest_number.py
+++ practice_python_batch_3/user_input_number_loop_display_the_lowest_number.py
@@ -1,15 +1,6 @@
 #Create a program that ask user to input a number, continue asking until the user input is invalid. Display the lowest number
 
-#same process
-#initialize
-#function
-#while loop
-#exit condition
-#input handling
-#append function
-#find the lowest number input
-
-number = []
+numbers = []
 
 while True:
     user_input = input("Enter a number: ")
@@ -17,18 +8,17 @@ while True:
     if user_input == 'l':
         break
 
- try:
-    num = int(user_input)
-    numbers.append(num)
+    try:
+        num = int(user_input)
+        numbers.append(num)
 
     except ValueError:
-        print("Invalid input! Please enter a valid number.")
+        print("Invalid input, enter a valid number.")
     
-    if numbers:
-    low_num = min(numbers)
-    print("The lowest number is: {low_num}")
+if numbers:
+    lowest_number = min(numbers)
+    print(f"The lowest number is: {lowest_number}")
 else:
-    print("No valid num")
+    print("No valid number")
 
-#fix indentions bro wyd
 print("type 'l' to display the lowest number")

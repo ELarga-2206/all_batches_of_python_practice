abcce5ec79c68a65a32ebba074820628c618838e
Elarga-2206 <earl.larga22@gmail.com>
2025-03-12 17:36:02 +0800

    added a working while loop

--- practice_python_batch_3/display_unique_if_number_is_unique_duplicate_if_not.py
+++ practice_python_batch_3/display_unique_if_number_is_unique_duplicate_if_not.py
@@ -1,7 +1,20 @@
 #Create a program that ask user to input a number, continue asking until the user input is invalid. Display "Unique" after input when the inputted number don't have duplicate. Display "Duplicate" after input when the inputted number have duplicate.
 
 #initialize empty list
+
 #use infinite loop/while loop
+
+numbers = []
+
+print("Enter numbers (type 'exit' to stop):")
+
+while True:
+    user_input = input("Enter a number: ")
+
+    if user_input.lower() == 'exit':
+        print("Exiting the program.")
+        break
+
 #ask for user number input
 #check for duplicates in user input:
     #print "duplicate"
@@ -10,4 +23,4 @@
 #check if the user input is invalid:
 #end
 
-Start
+

c7c4b6184449a88d04fa45ecbe3fd7c4ca170ccd
Elarga-2206 <earl.larga22@gmail.com>
2025-03-12 22:31:33 +0800

    replaced total_sum += 1 to total_sum += num, to prevent displaying 1 as avg, replaced 'int' with 'float' for decimal

--- practice_python_batch_4/display_average.py
+++ practice_python_batch_4/display_average.py
@@ -1,16 +1,5 @@
 # Create a program that ask user to input a number, continue asking until the user input is invalid. Display the average.
 
-#average
-# user inputs + user inputs / no of user inputs
-# must have counter of user inputs
-# must add the number to the list and update the total sum
-
-# counter = 0
-#while loop
-#user input
-#append valid inputs, then add their values
-#once added should be divided to total of inputs (valid numbers only, no alphabet)
-
 total_sum = 0
 numbers = []
 
@@ -23,9 +12,9 @@ while True:
         break
 
     try:
-        num = int(user_input)
+        num = float(user_input)
         numbers.append(num)
-        total_sum += 1
+        total_sum += num
 
     except ValueError:
         print("Invalid input! Please enter a valid number.")

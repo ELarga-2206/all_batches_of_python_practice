dacc10559d8223c50de43f15717f897962c26dfc
Elarga-2206 <earl.larga22@gmail.com>
2025-03-13 00:00:41 +0800

    reusing the code from a similar problem in batch3

--- practice_python_batch_4/display_number_highest_to_lowest.py
+++ practice_python_batch_4/display_number_highest_to_lowest.py
@@ -1,1 +1,22 @@
 # Create a program that ask user to input a number, continue asking until the user input is invalid. Display the number from highest to lowest. Clue: sort() function
+
+numbers = []
+
+print("Enter numbers: ")
+print("type 's' to sort numbers")
+
+while True:
+    user_input = (input("Enter a number: "))
+    if user_input == "s":
+        break
+
+    try:
+        num = int(user_input)
+        numbers.append(num)
+
+    except ValueError:
+        print("Invalid input! Please enter a valid number.")
+
+numbers.sort()
+for number in numbers:
+    print(number)
